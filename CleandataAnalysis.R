725080f7337b5c35c2e2ad1455af6f9a8ee78647
marcobejar <marcobejar77@gmail.com>
2020-10-18 13:53:53 -0500

    An increment with cleaning.

--- CleandataAnalysis.R
+++ CleandataAnalysis.R
@@ -3,8 +3,9 @@
 ################################### LOAD DATA ##################################
 ################################################################################
 ## First we download the data and we accommodate it in a directory I try
-## to do this in a way taht could fit if you already have run this script.
+## to do this in a way that could fit if you already have run this script.
 ################################################################################
+
 require("data.table")
 require("dplyr")
 
@@ -32,27 +33,79 @@ if(!file.exists(Zip.File.Destiny)){
 ## 3) The subjects for each row in 1) which are contained in subject_train.txt 
 ## and subject_train.txt
 ################################################################################
+getWearableComputingDS<-function(pathToData,
+                                 pathMainDir,
+                                 label,
+                                 fileSubject,
+                                 fileData,
+                                 fileActivities)
+{
+  Data<-fread(file.path(pathToData,fileData))
+  names(Data)<-fread(file.path(pathMainDir,"features.txt"))[[2]]
+  
+  Data.Subjects<-fread(file.path(pathToData,fileSubject))
+  names(Data.Subjects)<-c("Subject")
+  
+  Data.Subjects<-mutate(Data.Subjects,Type=label,.before="Subject")
+  
+  Data.Activities<-fread(file.path(pathToData,fileActivities))
+  names(Data.Activities)<-c("Activity")
+  
+  cbind(Data.Subjects,Data.Activities,Data)
+}
 
+cleanColumns<-function(dataToProcess){
+  Features.Names<-fread(file.path(Path.Main,"features.txt"))
+  Columns.Good<-grep("(std\\())|(mean\\())",Nombres$V2,value = TRUE)
+  select(Data.Total,"Type","Subject","Activity",columnsGood)
+}
 
+setActivitiesAsStrings<-function(dataToProcess){
+  Data.Activities<-fread(file.path(Path.Main,"activity_labels.txt"))
+  Data.Temp<-mutate(dataToProcess,
+         ActivityName=featuresdata$V2[match(Data.Total$Activity,featuresdata$V1)],
+         .before="Subject")
+  Data.Temp<-mutate(Data.Temp,Activity=NULL)
+  Data.Temp
+}
+
+# Now I get the datasets
 
 Path.Main <- file.path(Directory.Name,"UCI HAR Dataset")
 Path.Train <- file.path(Path.Main, "train")
 Path.Test <- file.path(Path.Main, "test")
 
-Data.Train<-fread(file.path(Path.Train,"X_train.txt"))
-names(Data.Train)<-fread(file.path(Path.Main,"features.txt"))[[2]]
+Data.Train<-getWearableComputingDS(Path.Train,
+                                   Path.Main,
+                                   "Train",
+                                   "subject_train.txt",
+                                   "X_train.txt",
+                                   "y_train.txt")
 
-Data.Train.Subjects<-fread(file.path(Path.Train,"subject_train.txt"))
-names(Data.Train.Subjects)<-c("Subject")
+Data.Test<-getWearableComputingDS(Path.Test,
+                                   Path.Main,
+                                   "Test",
+                                   "subject_test.txt",
+                                   "X_test.txt",
+                                   "y_test.txt")
 
-Data.Train.Activities<-fread(file.path(Path.Train,"y_train.txt"))
-names(Data.Train.Activities)<-c("Activity")
 
-Data.Train<-cbind(Data.Train.Subjects,Data.Train.Activities,Data.Train)
+#Merge both datasets
+Data.Total<-rbind(Data.Train,Data.Test)
 
-#Data.Train<-fread(file.path(Path.Train,"subject_train.txt"))
-#Data.Train<-cbind(Data.Train, fread(file.path(Path.Train,"y_train.txt")))
-#Data.Train<-cbind(Data.Train, fread(file.path(Path.Train,"X_train.txt")))
+# now I eliminate what is not needed, the incise 2 says 
+# "Extracts only the measurements on the mean and standard
+# deviation for each measurement. "
+
+Data.Total<-cleanColumns(Data.Total)
+
+#I set the name of the activity as something moredescriptive (incise 4) using
+# activity_labels.txt file.
+
+Data.Total<-setActivitiesAsStrings(Data.Total)
+
+
+
 
 
 
